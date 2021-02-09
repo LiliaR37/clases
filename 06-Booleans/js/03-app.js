d74a3944e12c0f7f437278472ec7f27c2602e6cf
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-02-09 13:21:03 -0300

    booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,13 @@
+//Buena forma de comparar sin agregar ya que se da por entendido la comparacion será True o False
+
+const autenticado = false;
+
+if(autenticado){
+    console.log('Si puedes ver netflix')
+} else {
+    console.log('No no puedes verlo')
+}
+
+//Operador ternario
+
+console.log(autenticado ? 'Si estas autenticado' : 'No estas autenticado');
